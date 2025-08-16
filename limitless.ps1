b0c7da3bd2f1b9a0da24be7498c9ed60465052c6
Satoru.Lua <132266167+HazemYoriichi@users.noreply.github.com>
2025-08-16 21:39:28 +0300

    Update limitless.ps1

--- limitless.ps1
+++ limitless.ps1
@@ -4,7 +4,7 @@ param
     [switch]$mirror
 )
 
-function Run-SpotX {
+function Run-LimitlessLegacy {
 
     param(
         [string]$params
@@ -14,11 +14,13 @@ function Run-SpotX {
     $retryInterval = 5
 
     if ($mirror) { 
-        $url = 'https://spotx-official.github.io/SpotX/run.ps1' 
+        # النسخة mirror (ممكن تعمل نسخة تانية على GitHub باسم mirror لو حابب)
+        $url = 'https://raw.githubusercontent.com/HazemYoriichi/Spotify-Patch/refs/heads/main/limitless.ps1' 
         $params += " -m"
     }
     else {
-        $url = 'https://raw.githubusercontent.com/SpotX-Official/SpotX/main/run.ps1'
+        # النسخة الرئيسية اللي هتكون رافعها عندك على GitHub
+        $url = 'https://raw.githubusercontent.com/HazemYoriichi/Spotify-Patch/refs/heads/main/limitless.ps1'
     }
 
     for ($retry = 1; $retry -le $maxRetryCount; $retry++) {
@@ -35,17 +37,17 @@ function Run-SpotX {
             return
         }
         else {
-            Write-Host ("Attempt $($retry): HTTP status code: $($StatusCode). Retrying in $($retryInterval) seconds...")
+            Write-Host ("[Limitless Legacy] Attempt $($retry): HTTP status code: $($StatusCode). Retrying in $($retryInterval) seconds...")
             Start-Sleep -Seconds $retryInterval
         }
     }
 
     Write-Host
-    Write-Warning "Failed to make the request after $maxRetryCount attempts, script terminated."
+    Write-Warning "[Limitless Legacy] Failed to make the request after $maxRetryCount attempts, script terminated."
     Write-Host $Error[0].Exception.Message
     pause
     exit
 }
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12;
-Run-SpotX -params $args
+Run-LimitlessLegacy -params $args
